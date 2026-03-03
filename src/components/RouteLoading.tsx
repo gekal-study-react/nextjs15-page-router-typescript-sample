c8ea4670bf2e69c1318b776ff062720ece843a7f
gekal <liu.hongying@hotmail.com>
2026-03-03 22:36:10 +0900

    Enhance `RouteLoading` to handle additional router events (`beforeHistoryChange`, `hashChangeStart`, `hashChangeComplete`) and improve logging for better debugging.

--- src/components/RouteLoading.tsx
+++ src/components/RouteLoading.tsx
@@ -11,22 +11,44 @@ export const RouteLoading: React.FC<RouteLoadingProps> = ({children}) => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const handleStart = (url: string) => {
+    const handleRouterChangeStart = (url: string, {shallow}: { shallow: boolean }) => {
+      console.log("route change start", url, {shallow})
       if (url !== router.asPath) {
         setLoading(true);
       }
     };
-    const handleComplete = () => setLoading(false);
-    const handleError = () => setLoading(false);
+    const handleRouterChangeComplete = (url: string, {shallow}: { shallow: boolean }) => {
+      console.log("route change complete", url, {shallow})
+      setLoading(false);
+    };
+    const handleRouterChangeError = (err: any, url: string, {shallow}: { shallow: boolean }) => {
+      console.log("route change error", err, url, {shallow})
+      setLoading(false);
+    };
+    const handleHashChangeStart = (url: string, {shallow}: { shallow: boolean }) => {
+      console.log("hash change start", url, {shallow})
+    };
+    const handleBeforeHistoryChange = (url: string, {shallow}: { shallow: boolean }) => {
+      console.log("before history change", url, {shallow})
+    };
+    const handleHashChangeComplete = (url: string, {shallow}: { shallow: boolean }) => {
+      console.log("hash change complete", url, {shallow})
+    };
 
-    router.events.on('routeChangeStart', handleStart);
-    router.events.on('routeChangeComplete', handleComplete);
-    router.events.on('routeChangeError', handleError);
+    router.events.on('routeChangeStart', handleRouterChangeStart);
+    router.events.on('routeChangeComplete', handleRouterChangeComplete);
+    router.events.on('routeChangeError', handleRouterChangeError);
+    router.events.on('beforeHistoryChange', handleBeforeHistoryChange);
+    router.events.on('hashChangeStart', handleHashChangeStart);
+    router.events.on('hashChangeComplete', handleHashChangeComplete);
 
     return () => {
-      router.events.off('routeChangeStart', handleStart);
-      router.events.off('routeChangeComplete', handleComplete);
-      router.events.off('routeChangeError', handleError);
+      router.events.off('routeChangeStart', handleRouterChangeStart);
+      router.events.off('routeChangeComplete', handleRouterChangeComplete);
+      router.events.off('routeChangeError', handleRouterChangeError);
+      router.events.off('beforeHistoryChange', handleBeforeHistoryChange);
+      router.events.off('hashChangeStart', handleHashChangeStart);
+      router.events.off('hashChangeComplete', handleHashChangeComplete);
     };
   }, [router]);
 
