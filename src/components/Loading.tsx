100b89f51261a5ffb48a8429cc1563377213139c
gekal <liu.hongying@hotmail.com>
2026-03-07 08:06:40 +0900

    Extract `AppBackdrop` component and refactor `Loading` component to reuse it.

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,16 +1,9 @@
 import React from "react";
-import { Backdrop, CircularProgress } from "@mui/material";
+import { CircularProgress } from "@mui/material";
+import { AppBackdrop } from "./AppBackdrop";
 
 export const Loading: React.FC = () => (
-  <Backdrop
-    sx={{
-      color: "#fff",
-      zIndex: (theme) => theme.zIndex.drawer + 1,
-      flexDirection: "column",
-      gap: 2,
-    }}
-    open={true}
-  >
+  <AppBackdrop open={true}>
     <CircularProgress color="inherit" />
-  </Backdrop>
+  </AppBackdrop>
 );
